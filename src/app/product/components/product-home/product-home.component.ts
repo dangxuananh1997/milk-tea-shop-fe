92a1d2dba5b63e326faddae60e4dbead313a22e6
Quân <qmup@users.noreply.github.com>
2018-07-19 12:18:05 +0700

    Add link to logo/ Customize scrollbar/Edit input color when login failed

--- src/app/product/components/product-home/product-home.component.ts
+++ src/app/product/components/product-home/product-home.component.ts
@@ -83,7 +83,7 @@ export class ProductHomeComponent implements OnInit {
     modalRef.componentInstance.productId = productId;
     modalRef.result.then(
       () => { },
-      (error) => { console.log(error); }
+      () => { }
     );
   }
 
